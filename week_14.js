ac96e8a2e76dfb0402c69a3f5239fa15f19ddf28
NL <Lapina.n.b@mail.ru>
2021-09-14 03:39:28 +1000

    т

---
+++ week_14.js
@@ -0,0 +1,15 @@
+
+function clickMe(){
+    let Number1Input = document.getElementById('Number1');
+    console.log(Number1Input.value)
+    }
+
+function clickMe(){
+    let Number2Input = document.getElementById('Number2');
+    console.log(Number2Input.value)
+    }
+
+    function Sum()
+    {
+alert( Number(X) +Number(Y))
+}
